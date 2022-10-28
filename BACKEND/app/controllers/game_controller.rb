3f3ba16c4500adcdd538e30a7b8f4b68ec085270
Will Lovell <willard.lovell@icloud.com>
2022-10-27 22:21:27 -0600

    initial controller routing

--- BACKEND/app/controllers/game_controller.rb
+++ BACKEND/app/controllers/game_controller.rb
@@ -1,3 +1,16 @@
 class GameController < ApplicationController
+set :default_content_type, 'application/json'
+  
+	get '/games' do
+	  games = Game.all
+	  games.to_json
+	end
+
+	
+  
+  get '/reviews' do
+    reviews = Review.all
+    reviews.to_json
+  end
 
 end
