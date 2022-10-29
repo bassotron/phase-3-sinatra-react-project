170d9597d2445af97d21cd14e5a9c42a2d953e4d
Will Lovell <willard.lovell@icloud.com>
2022-10-28 23:36:35 -0600

    added state and useEffect to App.js

---
+++ FRONTEND/game_app/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import GameList from './components/GameList'
+import GameReviews from './components/GameReviews'
+import HomePage from './components/HomePage'
+
+
+function App() {
+  const [games, setGames] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:9292/games')
+      .then((response) => response.json())
+      .then((games) => {
+        setGames(games);
+      })
+  }, [])
+
+  const onAddGame = (newGame) => {
+    setGames([...games, newGame])
+  }
+
+
+
+  return (
+
+    <div>
+    <Router>
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/games" element={ <GameList />}/>
+        <Route path="/reviews" element={<GameReviews/>} />
+      </Routes>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
