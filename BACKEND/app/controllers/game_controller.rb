d776a122eb71d79fc6288f6364a38612e29f8b38
Will Lovell <willard.lovell@icloud.com>
2022-10-28 20:16:27 -0600

    updated CRUD methods in game_controller

--- BACKEND/app/controllers/game_controller.rb
+++ BACKEND/app/controllers/game_controller.rb
@@ -1,16 +1,51 @@
 class GameController < ApplicationController
 set :default_content_type, 'application/json'
-  
-	get '/games' do
-	  games = Game.all
-	  games.to_json
-	end
-
+ games CRUD 
 	
+ get 'games' do
+	games = Game.all
+	games.to_json
+end
+
+post 'games' do
+	games = Game.create(title:params[:title], genre:params[:genre], platform:params[:platform])
+	games.to_json
+end
+	  
+patch 'games/:id' do  
+	games = Game.find(params[:id])
+	games.update(title:params[:title])
+	games.to_json
+end
+	  
+delete 'games/:id' do
+	games = Game.destroy(params[:id])
+	games.to_json
+end
+		
+	
+  #reviews CRUD 
+
+get '/reviews' do
+	reviews = Review.all
+	reviews.to_json
+end
   
-  get '/reviews' do
-    reviews = Review.all
-    reviews.to_json
-  end
+post '/reviews' do
+	reviews = Review.create(score:params[:score], comment:params[:comment], games_id:params[:games_id])
+	reviews.to_json
+end
+		
+patch 'reviews/:id' do  
+	reviews = Review.find(params[:id])
+	reviews.update(comment:params[:comment])
+	reviews.to_json
+end
+			
+delete 'reviews/:id' do
+	reviews = Review.destroy(params[:id])
+	reviews.to_json
+end
+		  
 
 end
